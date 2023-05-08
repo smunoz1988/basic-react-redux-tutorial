cb48dc2a5e16e9f5319a342f2c65eef1ec2c0d66
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-05-08 15:40:20 -0500

    add containers, modals and funcionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,27 @@
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+import Modal from "./components/Modal";
+import {useDispatch, useSelector} from 'react-redux'
+import { calculateTotals } from "./features/cart/cartSlice";
+import { useEffect} from "react";
+
 function App() {
-  return <h2>Redux Toolkit</h2>;
+const {cartItems} = useSelector((store) => store.cart)
+const {isOpen} = useSelector((store) => store.modal)
+
+const dispatch = useDispatch()
+ 
+useEffect(() => {
+  dispatch(calculateTotals())
+}, [cartItems])
+
+
+  return <main>
+    { isOpen &&   <Modal/> }
+  
+    <Navbar/>
+    <CartContainer/>
+
+  </main>;
 }
 export default App;
